fab92c8d0e45c85b176f9d1c461feede03aa2edb
Sners03 <reichelarslukas@gmail.com>
2023-11-30 19:41:11 +0100

    pdr ready

--- src/onboard_test.py
+++ src/onboard_test.py
@@ -4,12 +4,17 @@ import cv2
 import cv2.typing as cv2_type
 import itertools as iter
 import numpy as np
-import typing
+import os
+from time import time
+
 
 class onboardTest():
     def __init__(self, path) -> None:
         self.im =  self.__load_image(path)
+        self.width, self.height, channels = self.im.shape
         self.im_arr = np.array(self.im)
+        self.dirname = f"{time()}"
+        os.mkdir(self.dirname)
 
 
     def __load_image(self, path):
@@ -34,35 +39,52 @@ class onboardTest():
 
         return idx, np.count_nonzero(idx)
     
-    def mean_color_area(self, color:cv2_type.MatLike):
+    
+    def mean_color_area(self, color:cv2_type.MatLike, bias:float=0.1):
         #lower boder
-        lower_border = int(round(color.mean()*0.65))
+        lower_border = int(round(color.mean()*(1.0-bias)))
         #check isn't neccessary? always above 0?
         lower_border = 0 if lower_border<0 else lower_border #check higher than 0
         #upper border
-        upper_border = int(round(color.mean()*1.35))
+        upper_border = int(round(color.mean()*(1.0+bias)))
         upper_border = 255 if upper_border>255 else upper_border # check lower than 255
 
         color_area = ((color <= lower_border) | (color >= upper_border))
 
         return color_area
 
+    def __cutimage(self, x, y):
+        imcopy = self.im.copy()
+        roi = imcopy[max(0, y-25):min(y+25, self.width), max(0,x-25):min(self.height, x+50)]
+        return roi
 
+    def __find_rois(self, contours):
+        #init image var
+        for iter, contour in enumerate(contours):
+            x,y = contour[0][0]
+            filename = f"{iter}.png"
+            path = os.path.join(self.dirname, filename)
+            print(f"Creating file... {path}")
+            image = self.__cutimage(x,y)
+            cv2.imwrite(path, image)
     
-    
+   
     def shipfilter(self):
-        r, g, b = cv2.split(cv2.cvtColor(self.im, cv2.COLOR_BGR2RGB))[:3]
-        print(int(round(r.mean()*0.9)))
-        idx = (self.mean_color_area(r) & self.mean_color_area(g) & self.mean_color_area(b))
+        loadrgeb = cv2.COLOR_BGR2RGB
+        im = self.im
+        color = cv2.cvtColor(im, loadrgeb)
+        r, g, b = cv2.split(color)
+        idx = (self.mean_color_area(r) & self.mean_color_area(g) & self.mean_color_area(b, bias=0.35))
         waterfilter = idx.astype(np.uint8) * 255
 
         # Just for visual debugging
         cv2.imshow('image', self.im)
         cv2.imshow('water gone', waterfilter)
 
-        contours, hierarchy = cv2.findContours(waterfilter, cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(waterfilter, cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_TC89_KCOS)
 
         print(len(contours))
+        self.__find_rois(contours=contours)
         
         #cv2.rectangle(self.im, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
@@ -75,10 +97,15 @@ class onboardTest():
         #input()
         #cv2.imshow('red_mask', idx.astype(np.uint8) * 255)
 
-if __name__ == "__main__":
-    test = onboardTest(r"ship_images\02.png")
-    test.print_arr()
-    test.shipfilter()
+def mainrun():
+    if __name__ == "__main__":
+        test = onboardTest(r"src\ship_images\02.png")
+        print(test.im.shape)
+        test.shipfilter()
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+   mainrun()
